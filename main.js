7a81f546fdb7dfeda8ea0c75a9d18151cdc44070
technoturret <36348775+technoturret@users.noreply.github.com>
2018-12-02 21:09:09 -0500

    remove logging

--- main.js
+++ main.js
@@ -88,7 +88,7 @@ app.listen(process.env.PORT || 3000, function() {
 
 
 app.get("/notifiertester/:key/:id", function (req, res) {
-  console.log(req, res)
+  // console.log(req, res)
   let key = req.params.key
   
   if (!key || key !== process.env.accessKey) {
